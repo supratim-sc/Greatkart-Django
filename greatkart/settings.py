61f92abe8ce2b3bd1ad99d3bacaa1d9650ae90a4
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-11 18:31:57 +0530

    override global AUTH_USER_MODEL to our Custom user model

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -72,6 +72,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'greatkart.wsgi.application'
 
+AUTH_USER_MODEL = 'accounts.Account'
+
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
