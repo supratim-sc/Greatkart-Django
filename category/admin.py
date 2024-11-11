2e5f07d512af76c812114e8af30bf2c69f0b1190
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-11 23:50:47 +0530

    make slug field prepopulated and showing category image in admin panel

--- category/admin.py
+++ category/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Category
 
 # Register your models here.
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        'slug' : ['name']
+    }
+    list_display = ['name', 'slug', 'show_image']
+
+    # Custom function to show category image
+    def show_image(self, category):
+        # if category has any image
+        if category.image:
+            return format_html(f'<img src="{category.image.url}" alt="{category.name}" width="100" height="100" style="object-fit:contain">')
+        # if category does not have any image
+        else:
+            return 'No image'
+    
+admin.site.register(Category, CategoryAdmin)
