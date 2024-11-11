478bb33efa0634014dc796b6f49a76bba9dffe2c
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-11 16:22:53 +0530

    create Category model

--- category/models.py
+++ category/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='photos/categories', blank=True)
+
+    def __str__(self) -> str:
+        return self.name
