d395897fb86a2d37846dcf783d5aff6bc6fb588f
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-12 12:07:12 +0530

    filter products which are currently available and show them on homepage

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from store.models import Product
 
 def home(request):
-    return render(request, 'index.html')
+    products = Product.objects.filter(is_available=True)
+    context = {
+        'products' : products,
+    }
+    return render(request, 'index.html', context)
