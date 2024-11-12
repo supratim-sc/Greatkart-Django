270c45741dab4b4ac5ae44d485ec0a8f5dc3c257
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-12 16:49:26 +0530

    modify product_details view function and feed data in html template

--- store/views.py
+++ store/views.py
@@ -19,4 +19,12 @@ def store(request, category_slug=None):
     return render(request, 'store/store.html', context)
 
 def product_details(request, category_slug, product_slug):
-    return render(request, 'store/product_details.html')
+    try:
+        product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    
+    context = {
+        'product' : product,
+    }
+    return render(request, 'store/product_details.html', context)
