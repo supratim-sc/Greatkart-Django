cece92392f7837acf63a9e6cae34246840e86559
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-12 14:22:17 +0530

    implement slug based url to filter products based on category depending on slug provided

--- store/views.py
+++ store/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+from category.models import Category
 
 # Create your views here.
-def store(request):
-    products = Product.objects.filter(is_available=True)
+def store(request, category_slug=None):
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category, is_available=True)
+    else:
+        products = Product.objects.filter(is_available=True)
+    
     products_count = products.count()
-
     context = {
         'products' : products,
         'products_count' : products_count,
