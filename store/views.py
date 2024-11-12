f98a4c215da6e3af288e05c73e32b8216742f53c
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-12 12:54:43 +0530

    implement store url, store page and display products on store page

--- store/views.py
+++ store/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def store(request):
+    products = Product.objects.filter(is_available=True)
+    products_count = products.count()
+
+    context = {
+        'products' : products,
+        'products_count' : products_count,
+    }
+
+    return render(request, 'store/store.html', context)
