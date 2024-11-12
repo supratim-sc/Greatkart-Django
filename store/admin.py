9af94b7e2f0387b59b4a9c4523f4d78e0a5221cb
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-12 11:55:13 +0530

    create CustomProductAdmin class and make slug field as prepopulated

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+class CustomProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'stock', 'category', 'updated_at']
+    prepopulated_fields = {
+        'slug' : ['name'],
+    }
+
+    def __str__(self) -> str:
+        return self.name
+    
+admin.site.register(Product, CustomProductAdmin)
