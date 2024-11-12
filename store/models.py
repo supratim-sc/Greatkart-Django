52276ea368deeccdf80bba03b95e8ba0bda38614
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-12 10:16:15 +0530

    implement Product model in store app

--- store/models.py
+++ store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from category.models import Category
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    images = models.ImageField(upload_to='photos/products')
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
