f1757e991e29ce01014413dbf50f84d7bbe2bc1c
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-15 20:42:08 +0530

    implement added to cart and view cart button on the product details page if product is in cart

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.models import Cart, CartItem
+from carts.views import _get_cart_id
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -21,10 +23,14 @@ def store(request, category_slug=None):
 def product_details(request, category_slug, product_slug):
     try:
         product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        # checking if the product is in the cart or not
+        # as Cart is the foreignkry to the CartItem model, so we can access cart_id with __ (reverse relationship)
+        is_cart_item = CartItem.objects.filter(cart__cart_id=_get_cart_id(request), product=product).exists()
     except Exception as e:
         raise e
     
     context = {
         'product' : product,
+        'is_cart_item' : is_cart_item,
     }
     return render(request, 'store/product_details.html', context)
