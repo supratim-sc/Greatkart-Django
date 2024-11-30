2c158eec52afd0518cd556b77cdcd0c877790d11
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-30 16:49:46 +0530

    implement carts count context_processor to show cart items for logged in user

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -9,10 +9,16 @@ def get_cart_count(request):
         return {}
     
     try:
-        # as there will be only one cart with a cart_id so get() is used
-        cart = Cart.objects.get(cart_id=_get_cart_id(request))
-        # as there may be more than one cart items associated with a cart id, so filter() is used
-        cart_items = CartItem.objects.filter(cart=cart)
+        # if user logs in then fetch the cart item for that user
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user)
+
+        # is user not logged in then fetch cart items depending on the session id stored in the request
+        else:
+            # as there will be only one cart with a cart_id so get() is used
+            cart = Cart.objects.get(cart_id=_get_cart_id(request))
+            # as there may be more than one cart items associated with a cart id, so filter() is used
+            cart_items = CartItem.objects.filter(cart=cart)
         # iterating over all cart_items and add all their quantity together
         for cart_item in cart_items:
             cart_count += cart_item.quantity
