b04cd9aace5dbe56bffb43ad680c649e534a3640
Supratim Chakraborty <supratim12111996@gmail.com>
2024-12-01 18:39:36 +0530

    create Payment model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from accounts.models import Account
+
 # Create your models here.
+class Payment(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    payment_id = models.CharField(max_length=100)
+    payment_method = models.CharField(max_length=100)
+    amount_paid = models.CharField(max_length=100)  # this is the total amount to be paid by the user
+    status = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.payment_id
