9505021f14e3efea27b033200741153a25e1ecad
Supratim Chakraborty <supratim12111996@gmail.com>
2024-12-26 12:17:40 +0530

    Create CustomOrderAdmin model on Order model

--- orders/admin.py
+++ orders/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from .models import Payment, Order, OrderProduct
 
 # Register your models here.
+class CustomOrderAdmin(admin.ModelAdmin):
+    list_display = ['order_number', 'full_name', 'phone_number', 'email', 'order_total', 'status', 'is_ordered']
+    list_filter = ['status', 'is_ordered']
+    search_fields = ['order_number', 'first_name', 'last_name', 'phone_number', 'email']
+    list_per_page = 20
+
 admin.site.register(Payment)
 
-admin.site.register(Order)
+admin.site.register(Order, CustomOrderAdmin)
 
 admin.site.register(OrderProduct)
